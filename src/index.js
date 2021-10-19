6720c02716fcbf4d684c0a54f8a02e3ed5da024f
David Heinebäck <du222aa@student.lnu.se>
2021-10-19 08:21:38 +0200

    add: sentencetypes to export for npm

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import Document from './parser/Document.js'
 import Readline from './parser/ReadLine.js'
+import ExclamationSentence from './parser/sentence/ExclamationSentence.js'
+import RegularSentence from './parser/sentence/RegularSentence.js'
+import QuestionSentence from './parser/sentence/QuestionSentence.js'
 
 export default {
   Document,
-  Readline
+  Readline,
+  SentenceTypes: {
+    RegularSentence,
+    ExclamationSentence,
+    QuestionSentence
+  }
 }
